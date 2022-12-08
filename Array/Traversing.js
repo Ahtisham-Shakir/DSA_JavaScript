ebb5114e73f7e561a62e6a0353ceec9503769229
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-12-08 23:15:31 +0500

    Array Traversing and Accessing

---
+++ Array/Traversing.js
@@ -0,0 +1,10 @@
+// Traversing Means visiting every element in the array
+
+let data = [1, 23, 43, 20, 45, 21, 50];
+for (let i = 0; i < data.length; i++) {
+  console.log(`Array ${i} is ${data[i]}`);
+}
+
+// Accessing means accessing individual element from the array
+let index = 5;
+console.log(`element at ${index}th index is ${data[index]}`);
