73a3627d3c9099da1f0da1acdce8b516a426dd86
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2022-12-19 22:59:49 +0500

    sorting in decending order

--- Array/Sort/SelectionSort.js
+++ Array/Sort/SelectionSort.js
@@ -1,17 +1,51 @@
 // Selection sort
 
+// const arr = [20, 12, 53, 3];
+
+// for (let i = 0; i < arr.length; i++) {
+//   let min = i;
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[j] < arr[min]) {
+//       min = j;
+//     }
+//   }
+//   let temp = arr[i];
+//   arr[i] = arr[min];
+//   arr[min] = temp;
+// }
+
+// console.log("array after sorting in ascending order", arr);
+
+// Selection sort
+
+// const arr = [20, 12, 53, 3];
+
+// for (let i = 0; i < arr.length; i++) {
+//   let min = i;
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[j] < arr[min]) {
+//       min = j;
+//     }
+//   }
+//     let temp = arr[i];
+//     arr[i] = arr[min];
+//     arr[min] = temp;
+// }
+
+// console.log("array after sorting", arr);
+
 const arr = [20, 12, 53, 3];
 
 for (let i = 0; i < arr.length; i++) {
-  let min = i;
+  let max = i;
   for (let j = i + 1; j < arr.length; j++) {
-    if (arr[j] < arr[min]) {
-      min = j;
+    if (arr[j] > arr[max]) {
+      max = j;
     }
   }
   let temp = arr[i];
-  arr[i] = arr[min];
-  arr[min] = temp;
+  arr[i] = arr[max];
+  arr[max] = temp;
 }
 
-console.log("array after sorting in ascending order", arr);
+console.log("array after sorting in decending order", arr);
